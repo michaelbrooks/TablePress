d7a9728ce722ff4d33882fc4313ad60618a4fcc9
SCCL <sccl@uw.edu>
2014-04-09 00:03:50 -0700

    working update with no info box

--- views/view-editsccl.php
+++ views/view-editsccl.php
@@ -53,11 +53,40 @@ class TablePress_Editsccl_View extends TablePress_Edit_View {
 	  remove_meta_box( "tablepress_{$this->action}-{$id}", null, $context);
 	}
 
+	/**
+	 * Print hidden field with a nonce for the screen's action, to be transmitted in HTTP requests
+	 *
+	 * @since 1.0.0
+	 * @uses wp_nonce_field()
+	 *
+	 * @param array $data Data for this screen
+	 * @param array $box Information about the text box
+	 */
+	protected function action_nonce_field( array $data, array $box ) {
+	  // use custom nonce field here, that includes the table ID
+	  // instead of using $this->action use 'edit' to fool the controller later
+	  wp_nonce_field( TablePress::nonce( 'edit', $data['table']['id'] ), 'nonce-edit-table' ); echo "\n";
+	  wp_nonce_field( TablePress::nonce( 'preview_table', $data['table']['id'] ), 'nonce-preview-table', false, true );
+	}
 
-	public function postbox_table_information( $data, $box ) {
+        public function postbox_table_data($data, $box) {
+	  parent::postbox_table_data($data, $box);
+
+	  if (!current_user_can('administrator')) {
+	    $this->postbox_hidden_fields($data);
+	  }
+	}
+
+	public function postbox_hidden_fields($data) {
 ?>
-asdf
+<div class="hidden-fields-from-table-information">
+  <input type="hidden" name="table[id]" id="table-id" value="<?php echo esc_attr( $data['table']['id'] ); ?>" />
+  <input type="hidden" name="table[new_id]" id="table-new-id" value="<?php echo esc_attr( $data['table']['id'] ); ?>"/>
+  <input type="hidden" id="table-information-shortcode" value="<?php echo esc_attr( '[' . TablePress::$shortcode . " id={$data['table']['id']} /]" ); ?>" />
+  <input type="hidden" name="table[name]" id="table-name" value="<?php echo esc_attr( $data['table']['name'] ); ?>" />
+  <input type="hidden" name="table[description]" id="table-description" value="<?php echo esc_textarea( $data['table']['description'] ); ?>" />
+</div>
 <?php
-        }
+	}
 
 } // class TablePress_Editsccl_View
